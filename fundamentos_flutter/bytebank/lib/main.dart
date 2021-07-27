0bfbaabe29ee435098cebb90f5bd37cfd2c4f776
allan-mlpe <allan_mlpe@hotmail.com>
2021-07-26 21:04:58 -0300

    Adicionando ListTile com cards

--- fundamentos_flutter/bytebank/lib/main.dart
+++ fundamentos_flutter/bytebank/lib/main.dart
@@ -4,6 +4,24 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100,00'),
+                subtitle: Text('1000'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('2800,00'),
+                subtitle: Text('1000'),
+              ),
+            )
+          ],
+        ),
         appBar: AppBar(
           title: Text('Transferências'),
         ),
