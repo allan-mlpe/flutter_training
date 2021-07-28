df016625767c8fc15b6875d9b96af137713abd17
allan-mlpe <allan_mlpe@hotmail.com>
2021-07-28 16:09:52 -0300

    Criando formulário de cadastro de novo contato

--- persistencia_flutter/README.md
+++ persistencia_flutter/README.md
@@ -1,16 +1,22 @@
-# persistencia_flutter
+# Módulo de Persistência
 
-A new Bytebank project.
+## Conceitos úteis apresentados
 
-## Getting Started
+- Criar navegação entre rotas:
+```
+Navigator.of(context).push(
+    MaterialPageRoute(
+        builder: (context) => WidgetAlvo()
+    )
+);
+````
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- Adicionar atributos como altura e largura em Widgets que não tem essas propriedades:
+```
+// podemos envolver o componente com `SizedBox`
+SizedBox(
+  width: ...,
+  height: ...,
+  child: WidgetQualquer(...),
+)
+```
--- persistencia_flutter/lib/screens/formulario_novo_contato.dart
+++ persistencia_flutter/lib/screens/formulario_novo_contato.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 
 const String TITULO_FORMULARIO_NOVO_CONTATO = 'Novo Contato';
 
+const String LABEL_NOME_CONTATO = 'Nome do contanto';
+const String LABEL_NUMERO_CONTA = 'Número da conta';
+
+const String TITULO_BOTAO_CADASTRAR = 'Cadastrar';
+
+const double TAMANHO_FONTE_INPUT = 24.0;
+
 class FormularioNovoContato extends StatelessWidget {
 
   @override
@@ -9,6 +16,43 @@ class FormularioNovoContato extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(TITULO_FORMULARIO_NOVO_CONTATO),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: LABEL_NOME_CONTATO,
+              ),
+              style: TextStyle(
+                fontSize: TAMANHO_FONTE_INPUT
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: LABEL_NUMERO_CONTA,
+                ),
+                style: TextStyle(
+                  fontSize: TAMANHO_FONTE_INPUT
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: SizedBox(
+                width: double.maxFinite,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(TITULO_BOTAO_CADASTRAR),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
