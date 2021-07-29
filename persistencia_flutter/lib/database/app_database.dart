e242e54704c15b180c81a1924c0eeacb3ddab748
allan-mlpe <allan_mlpe@hotmail.com>
2021-07-29 17:38:01 -0300

    Refatorando para utilizar async e await

--- persistencia_flutter/lib/database/app_database.dart
+++ persistencia_flutter/lib/database/app_database.dart
@@ -2,46 +2,43 @@ import 'package:path/path.dart';
 import 'package:persistencia_flutter/models/contato.dart';
 import 'package:sqflite/sqflite.dart';
 
-Future<Database> criarBancoDeDados(){
-  // `getDatabasePath` retorna um Future, por isso usamos o `then`
-  return getDatabasesPath().then((dbPath){
-    final String path = join(dbPath, 'bytebank.db');
-    return openDatabase(
-      path,
-      onCreate: (db, version) {
-      db.execute('CREATE TABLE contatos('
-          'id INTEGER PRIMARY KEY, '
-          'nome TEXT, '
-          'numero_conta INTEGER)');
-      },
-      version: 1,
-      //onDowngrade: onDatabaseDowngradeDelete,
+Future<Database> getDatabase() async {
+  final String dbPath = await getDatabasesPath();
+  final String path = join(dbPath, 'bytebank.db');
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+    db.execute('CREATE TABLE contatos('
+        'id INTEGER PRIMARY KEY, '
+        'nome TEXT, '
+        'numero_conta INTEGER)');
+    },
+    version: 1,
+    //onDowngrade: onDatabaseDowngradeDelete,
+  );
+}
+
+Future<int> salvarContato(Contato contato) async {
+  final Database db = await getDatabase();
+  final Map<String, dynamic> contatoMap = Map();
+  contatoMap['nome'] = contato.nome;
+  contatoMap['numero_conta'] = contato.numeroConta;
+  return db.insert('contatos', contatoMap);
+}
+
+Future<List<Contato>> buscarContatos() async {
+  final Database db = await getDatabase();
+  final List<Map<String, dynamic>> resultado = await db.query('contatos');
+
+  final List<Contato> contatos = [];
+
+  for (Map<String, dynamic> linha in resultado) {
+    final Contato contato = Contato(
+      linha['id'],
+      linha['nome'],
+      linha['numero_conta'],
     );
-  });
-}
-
-Future<int> salvarContato(Contato contato) {
-  return criarBancoDeDados().then((db) {
-    final Map<String, dynamic> contatoMap = Map();
-    contatoMap['nome'] = contato.nome;
-    contatoMap['numero_conta'] = contato.numeroConta;
-    return db.insert('contatos', contatoMap);
-  });
-}
-
-Future<List<Contato>> buscarContatos() {
-  return criarBancoDeDados().then((db) {
-    return db.query('contatos').then((maps) {
-      final List<Contato> contatos = [];
-      for (Map<String, dynamic> map in maps) {
-        final Contato contato = Contato(
-          map['id'],
-          map['nome'],
-          map['numero_conta'],
-        );
-        contatos.add(contato);
-      }
-      return contatos;
-    });
-  });
+    contatos.add(contato);
+  }
+  return contatos;
 }
